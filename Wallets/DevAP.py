b004b51905ea01db597295a47143cde7f96c531c
Andres Hyer <hyerap@vcu.edu>
2020-01-06 16:22:00 -0500

    Developer information (README.md info)

--- Wallets/DevAP.py
+++ Wallets/DevAP.py
@@ -10,7 +10,7 @@ https://github.com/andreshyer/OfflinePaperWallet.
 General Information:
 All the private key and public address that generated in application are 
 generated using python built-in packages as well as the external packages: 
-ecdsa, base58, and pysha3. Also this application is completely self-contained. 
+ecdsa, base58, and Crypto. Also this application is completely self-contained. 
 Meaning the machine never has to be connected to the internet for this 
 application to work. 
 
@@ -47,6 +47,16 @@ int returned is 20191410454561. So the new string that is feed into the SHA256 p
 Since the random number nor the exact time and date can not be reproduced, that makes the 
 private key generated non-recoverable. 
 
+Mnemonic Seed, electrum based:
+An mnemonic seed is a way to encode a private key in a user friendly fashion. 
+So instead of writing down a private key where the user could easily mis-write 
+a letter or number, the user can write down a mnemonic which can be decoded as 
+a private key. So in other words, a mnemonic seed is just a representation of 
+the private key. The protocol to generate a mnemonic seed is. 
+
+private key > mnemonic encode > mnemonic seed (24 words)
+mnemonic seed (24 words) > append checksum > mnemonic seed (25 words)
+
 Private key to public address:
 This section will attempt to briefly explain the idea of how the public addresses are 
 generated, in this script. Bitcoin (BTC) and Litecoin (LTC) follow extremely, 
@@ -75,6 +85,31 @@ wallet are:
 private key > SECP256k1 elliptic curve > public key |
 public key > keccak > last 20 bytes from hash > add extension "0x" > ETH address
 
+Monero is the most complicated wallet to generate from the wallets in this
+application. There are multiple protocols to generate a monero wallet.
+The most popular, and the protocol used in this application, is to generate the
+keys from a electrum based mnemonic seed. 
+
+private key > sc_reduce32 > private spend key
+private spend key > keccak_256 > sc_reduce32 > private view key
+
+private view key > ed25519 > public view key
+private spend key > ed25519 > public spend key
+
+public address data = b"18" + public spend key + public view key
+public address > keccak_256 > checksum
+public address data = public address + first four byte of checksum
+
+The string is converted to Base58. It is not done 
+all at once like a Bitcoin address, but in 8-byte blocks. This gives us 
+eight full-sized blocks and one 5-byte block that are appended together, 
+to generate the full public address
+
+public address data > base58 chunk coding > public address
+
+I found this protocol from Cryptonote's website, but an easy to digest place to 
+test the protocol can be found here https://xmr.llcoins.net/addresstests.html. 
+
 To implement these protocols into python, I used the package ecdsa for the 
 SECP256k1 curve, Crypto for the keccak curve, and hashlib for the SHA256 hashing 
 algorithm. Also binascii was used to decode and encode hex formatted numbers
@@ -86,15 +121,16 @@ While this tool is 'safe', it is obvious that you should trust no one, especiall
 when crypto-currency is involved. I would personally recommend testing a few keys 
 that you do not intend to use on the different blockchains to make sure that they 
 work fine. It is strongly encourage for you too look through the source code and 
-make sure they are no obviously buggy/malicious code. The code is pretty simple, 
+make sure they are no obviously buggy/malicious code. The code is /fairly/ simple, 
 anyone can look and see there are no online sources being used and that I am not 
-just sending information to myself or anyone else.
+just sending information to myself or anyone else, or just randomly choosing private
+keys from a list of private keys.
 
 Upcoming developments
 These are changes that I would like to add in the near future.
-1) Option for more cryto-currencies (Most notably Monero)
-2) Support for Linux/Mac (Mainly Linux)
-3) More fluid/modern GUI
+1) Mnemonic Seed for Ethereum
+2) Option for more cryto-currencies (Suggestions welcomed!)
+3) Support for Linux/Mac (Mainly Linux, I do not own a Mac)
 4) Better Help Screen(s)
 
 Where can I donate?
